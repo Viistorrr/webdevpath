f1f6c827a0b8899e77c84fc303e0e856330addb7
viistorrr <victormeza41@gmail.com>
2022-10-08 23:02:33 -0500

    working in add modulos

--- src/pages/progress/[user].tsx
+++ src/pages/progress/[user].tsx
@@ -1,24 +1,79 @@
-import { doc, getDoc, query, where, collection } from "firebase/firestore";
-import MainStats from "@components/Dashboard/Stats";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  query,
+  where,
+  collection,
+} from "firebase/firestore";
+
 import Layout from "@components/Layout";
 import { db } from "@config/firebase";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+
 /*TODO: Form to Add modules and activities by user*/
 
 const Index = ({ mentoring }: any): JSX.Element => {
+  const addMentoring = async () => {
+    const cityRef = doc(db, "mentorship", "documentMentor");
+    setDoc(
+      cityRef,
+      {
+        modules: {
+          name: "modulo 1",
+          completed: false,
+          order: 1,
+          activities: {
+            name: "actividad 1",
+            link: "http://www.viistorrr.com/webdevpath",
+            completed: false,
+            order: 1,
+          },
+        },
+      },
+      { merge: true }
+    );
+    /*  const docRef = await setDoc(doc(db, "mentorship", "documentMentor"), {
+      name: "test add",
+      email: "test@email.com",
+    }); */
+  };
   return (
     <div>
       <Layout pageTitle="Info">
         {mentoring && (
           <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-            <div className="px-4 py-5 sm:px-6">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Applicant Information
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                Personal details and application.
-              </p>
+            <div className="flex">
+              <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  Applicant Information
+                </h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                  Personal details and application.
+                </p>
+              </div>
+              <div className="flex px-4 py-5 sm:px-6 gap-6">
+                <div>
+                  <label htmlFor="email" className="sr-only">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="block w-full h-full rounded-md border-sky-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
+                    placeholder="you@example.com"
+                  />
+                </div>
+                <div>
+                  <button
+                    onClick={() => addMentoring()}
+                    type="button"
+                    className="inline-flex items-center rounded-md border border-sky-700 bg-sky-100 px-6 py-3 text-base font-medium text-sky-700 hover:bg-sky-200 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                  >
+                    Agregar Módulo
+                  </button>
+                </div>
+              </div>
             </div>
             <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
               <dl className="sm:divide-y sm:divide-gray-200">
